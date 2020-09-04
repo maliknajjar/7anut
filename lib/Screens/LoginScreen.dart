c4f8dd58c1a06b0e9099f24555939d404ab3c335
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2020-09-04 07:42:35 +0100

    some improvemnts in design in the login page

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -4,137 +4,218 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              // color: Colors.blue[400],
-              gradient: RadialGradient(
-                colors: [
-                  Color(0xFF5DA7E6).withOpacity(0.75),
-                  Color(0xFF3C9DE5),
-                ],
-                // begin: FractionalOffset(0.0, 0.0),
-                // end: FractionalOffset(0.0, 1.0),
-                stops: [0.0, 1.0],
-                radius: 1,
-                tileMode: TileMode.clamp
-              ),
-            ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: [
+              Color(0xFF5DA7E6).withOpacity(0.75),
+              Color(0xFF3C9DE5),
+            ],
+            // begin: FractionalOffset(0.0, 0.0),
+            // end: FractionalOffset(0.0, 1.0),
+            stops: [0.0, 1.0],
+            radius: 1,
+            tileMode: TileMode.clamp
           ),
-          Center(
-            child: Container(
-              constraints: BoxConstraints(maxWidth: 300),
-              height: 200,
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              decoration: BoxDecoration(
-                color: Colors.yellow,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15)
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                constraints: BoxConstraints(maxWidth: 300),
+                // height: 200,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15)
+                  ),
+                  boxShadow: [
+                      BoxShadow(
+                      blurRadius: 10,
+                      spreadRadius: 3,
+                      color: Colors.grey[800].withOpacity(0.5)
+                    ),
+                  ]
                 ),
-                boxShadow: [
-                    BoxShadow(
-                    blurRadius: 10,
-                    spreadRadius: 3,
-                    color: Colors.grey[800].withOpacity(0.5)
-                  ),
-                ]
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 25, bottom: 25),
+                      child: Image.asset(
+                        "assets/images/logo-01.png",
+                        height: 35,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                            BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 0,
+                            color: Colors.black.withOpacity(0.1)
+                          ),
+                        ],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xFFFFFCE1),
+                            Color(0xFFFFF6A4),
+                          ],
+                          begin: FractionalOffset(0.0, 0.0),
+                          end: FractionalOffset(0.0, 1.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp
+                        ), 
+                      ),
+                      child: TextField(
+                        style: TextStyle(fontSize: 15),
+                        cursorColor: Colors.black54,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          isDense: true,
+                          contentPadding: EdgeInsets.symmetric(vertical: 10),
+                          hintText: 'Email'
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: 7.5
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                            BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 0,
+                            color: Colors.black.withOpacity(0.1)
+                          ),
+                        ],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xFFFFFCE1),
+                            Color(0xFFFFF6A4),
+                          ],
+                          begin: FractionalOffset(0.0, 0.0),
+                          end: FractionalOffset(0.0, 1.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp
+                        ),
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                        cursorColor: Colors.black54,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          isDense: true,
+                          contentPadding: EdgeInsets.symmetric(vertical: 10),
+                          hintText: 'Password'
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.only(top: 20, bottom: 20),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                            BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 3,
+                            color: Colors.black.withOpacity(0.25)
+                          ),
+                        ],
+                        // border: Border.all(color: Colors.black, width: 2),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15)
+                        ),
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.yellow[300],
+                            Colors.yellow,
+                          ],
+                          begin: FractionalOffset(0.0, 0.0),
+                          end: FractionalOffset(0.0, 1.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp
+                        ), 
+                      ),
+                      child: Center(
+                        child: Text("Login"),
+                      ),
+                    )
+                  ],
+                ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
+              Row(
                 children: [
-                  Image.asset(
-                    "assets/images/logo-01.png",
-                    height: 35,
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: 25
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                          BoxShadow(
-                          blurRadius: 10,
-                          spreadRadius: 3,
-                          color: Colors.black.withOpacity(0.25)
-                        ),
-                      ],
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xFFFFFCE1),
-                          Color(0xFFFFF6A4),
-                        ],
-                        begin: FractionalOffset(0.0, 0.0),
-                        end: FractionalOffset(0.0, 1.0),
-                        stops: [0.0, 1.0],
-                        tileMode: TileMode.clamp
-                      ), 
-                    ),
-                    child: TextField(
-                      style: TextStyle(fontSize: 15),
-                      cursorColor: Colors.black54,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        isDense: true,
-                        contentPadding: EdgeInsets.symmetric(vertical: 10),
-                        hintText: 'Email'
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      height: 2,
+                      margin: EdgeInsets.only(left: 20, top: 30, bottom: 30),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
                       ),
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: 15
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      child: Center(child: Text("or")),
                     ),
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                          BoxShadow(
-                          blurRadius: 10,
-                          spreadRadius: 3,
-                          color: Colors.black.withOpacity(0.25)
-                        ),
-                      ],
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xFFFFFCE1),
-                          Color(0xFFFFF6A4),
-                        ],
-                        begin: FractionalOffset(0.0, 0.0),
-                        end: FractionalOffset(0.0, 1.0),
-                        stops: [0.0, 1.0],
-                        tileMode: TileMode.clamp
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      height: 2,
+                      margin: EdgeInsets.only(right: 20, top: 30, bottom: 30),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
                       ),
                     ),
-                    child: TextField(
-                      obscureText: true,
-                      style: TextStyle(
-                        fontSize: 15,
-                      ),
-                      cursorColor: Colors.black54,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        isDense: true,
-                        contentPadding: EdgeInsets.symmetric(vertical: 10),
-                        hintText: 'Password'
-                      ),
-                    ),
-                  )
+                  ),
                 ],
               ),
-            ),
+              Container(
+                width: double.infinity,
+                constraints: BoxConstraints(maxWidth: 300),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15)
+                  ),
+                  boxShadow: [
+                      BoxShadow(
+                      blurRadius: 10,
+                      spreadRadius: 3,
+                      color: Colors.grey[800].withOpacity(0.5)
+                    ),
+                  ]
+                ),
+                child: Text("test"),
+              ),
+            ],
           ),
-        ], 
+        ),
       ),
     );
   }
