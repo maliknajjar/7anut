785e8d19b694b73fd097737dfc0412a5f6292ff4
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2020-09-22 04:54:03 +0100

    adding tabs to category screem

--- lib/Screens/CategoryScreen.dart
+++ lib/Screens/CategoryScreen.dart
@@ -8,7 +8,7 @@ class CategoryScreen extends StatelessWidget {
     // var args = ModalRoute.of(context).settings.arguments;
 
     return DefaultTabController(
-      length: 3,
+      length: 14,
       child: Scaffold(
         appBar: AppBar(
         iconTheme: IconThemeData(
@@ -21,15 +21,41 @@ class CategoryScreen extends StatelessWidget {
         ),
         backgroundColor: Color(0xFF5DA7E6),
           bottom: TabBar(
+            labelColor: Color(0xFFFCF555),
+            unselectedLabelColor: Color(0xFF303030),
+            indicatorColor: Color(0xFFFCF555),
+            isScrollable: true,
             tabs: [
-              Tab(icon: Icon(Icons.directions_car)),
-              Tab(icon: Icon(Icons.directions_transit)),
-              Tab(icon: Icon(Icons.directions_bike)),
+              Tab(icon: Text("New products")),
+              Tab(icon: Text("water")),
+              Tab(icon: Text("Fruits and vegetables")),
+              Tab(icon: Text("Baked goods")),
+              Tab(icon: Text("Snacks")),
+              Tab(icon: Text("icecream")),
+              Tab(icon: Text("food")),
+              Tab(icon: Text("drinks")),
+              Tab(icon: Text("Basic materials")),
+              Tab(icon: Text("Personal care")),
+              Tab(icon: Text("Electronics")),
+              Tab(icon: Text("Animal food")),
+              Tab(icon: Text("kids Accessories")),
+              Tab(icon: Text("Clothes")),
             ],
           ),
         ),
         body: TabBarView(
           children: [
+            CategoryWidget(),
+            CategoryWidget(),
+            CategoryWidget(),
+            CategoryWidget(),
+            CategoryWidget(),
+            CategoryWidget(),
+            CategoryWidget(),
+            CategoryWidget(),
+            CategoryWidget(),
+            CategoryWidget(),
+            CategoryWidget(),
             CategoryWidget(),
             CategoryWidget(),
             CategoryWidget(),
