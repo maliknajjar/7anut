8b2b2a89cd0d0d86f07aa7345a8cfc6e9d58eddc
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-01-07 03:02:14 +0100

    progress

--- lib/Screens/GetLocationScreen.dart
+++ lib/Screens/GetLocationScreen.dart
@@ -6,8 +6,8 @@ import 'dart:async';
 class GetLocationScreen extends StatelessWidget {
   Completer<GoogleMapController> _controller = Completer();
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    target: LatLng(5, 20),
+    zoom: 11,
   );
 
   @override
