761b8fc97be250121b7550c544cdc41cbec3dadf
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-01-11 11:17:07 +0100

    adding zoom buttons to the map

--- lib/Screens/GetLocationScreen.dart
+++ lib/Screens/GetLocationScreen.dart
@@ -5,16 +5,50 @@ class GetLocationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String token = 'pk.eyJ1IjoibWFsaWs0NDY2NDQiLCJhIjoiY2tqc2FzNnM5M3kwdzJzbG9pZjNwaGhoYyJ9.fvy5js-0tXvMXh5SrJWwLA';
-    final String style = 'mapbox://styles/mapbox/light-v9';
+    final String style = 'mapbox://styles/malik446644/ckjsbj4s07hy619pa7hbgf2lq';
     
     return Scaffold(
-      body: MapboxMap(
-        accessToken: token,
-        styleString: style,
-        initialCameraPosition: CameraPosition(
-          zoom: 3.0,
-          target: LatLng(36.8065, 10.1815),
-        ),
+      body: Stack(
+        children: [
+          MapboxMap(
+            accessToken: token,
+            styleString: style,
+            initialCameraPosition: CameraPosition(
+              zoom: 3.0,
+              target: LatLng(36.8065, 10.1815),
+            ),
+          ),
+          Positioned(
+            bottom: 25,
+            right: 25,
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50)
+              ),
+              child: Center(
+                child: Text("+"),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 100,
+            right: 25,
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50)
+              ),
+              child: Center(
+                child: Text("+"),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
