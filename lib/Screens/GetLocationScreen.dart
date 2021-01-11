924e1984847ebfe163db794b45a2054004966333
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-01-11 06:23:07 +0100

    wooow

--- lib/Screens/GetLocationScreen.dart
+++ lib/Screens/GetLocationScreen.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:mapbox_gl/mapbox_gl.dart';
+// import 'package:mapbox_gl/mapbox_gl.dart';
 
 class GetLocationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        iconTheme: IconThemeData(
-          color: Color(0xFF303030),
-        ),
-        title: Text(
-          "Adresses",
-          style: TextStyle(
-            color: Color(0xFF303030),
-          ),
-        ),
-        backgroundColor: Color(0xFF5DA7E6),
-      ),
-      body: MapboxMap(
-        accessToken: "pk.eyJ1IjoibWFsaWs0NDY2NDQiLCJhIjoiY2tqcmFsNjh2M2N6bDJ0cW9xdXprM3dsciJ9.AJyHupCTovVbfLArWFDKEQ",
-        styleString: 'url-to-style',
-        initialCameraPosition: CameraPosition(
-          zoom: 15.0,
-          target: LatLng(14.508, 46.048),
-        ),
-      ),
+    return  Scaffold(
+      body: Text("data"),
     );
+    // Scaffold(
+    //   appBar: AppBar(
+    //     centerTitle: true,
+    //     iconTheme: IconThemeData(
+    //       color: Color(0xFF303030),
+    //     ),
+    //     title: Text(
+    //       "Adresses",
+    //       style: TextStyle(
+    //         color: Color(0xFF303030),
+    //       ),
+    //     ),
+    //     backgroundColor: Color(0xFF5DA7E6),
+    //   ),
+    //   body: MapboxMap(
+    //     accessToken: "pk.eyJ1IjoibWFsaWs0NDY2NDQiLCJhIjoiY2tqcmFsNjh2M2N6bDJ0cW9xdXprM3dsciJ9.AJyHupCTovVbfLArWFDKEQ",
+    //     styleString: 'url-to-style',
+    //     initialCameraPosition: CameraPosition(
+    //       zoom: 15.0,
+    //       target: LatLng(14.508, 46.048),
+    //     ),
+    //   ),
+    // );
   }
 }
