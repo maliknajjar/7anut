eacac69991ddda066f5354ebced13101f870515d
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-03-06 16:18:33 +0100

    progress

--- lib/env.dart
+++ lib/env.dart
@@ -2,5 +2,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 
 // ignore: camel_case_types
 class env {
-  static String apiUrl = kIsWeb ? "http://localhost:8000" : "http://192.168.1.40:8000";
+  static String apiUrl = kIsWeb 
+  ? "http://localhost:8000"         // for the web wich is in the same server location
+  : "http://192.168.1.40:8000";     // in the device
 }
