af2f945b6ae4840afc2ba9fecc84eae3117ec227
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-03-07 00:28:11 +0100

    added an icon for the web version

--- lib/env.dart
+++ lib/env.dart
@@ -3,6 +3,6 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 // ignore: camel_case_types
 class env {
   static String apiUrl = kIsWeb 
-  ? "http://localhost:8000"         // for the web wich is in the same server location
-  : "http://192.168.1.40:8000";     // in the device
+  ? "http://localhost"         // for the web wich is in the same server location
+  : "http://192.168.1.40";     // in the device
 }
