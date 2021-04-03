67d2944144a5544aed4086745f2f4aea96471600
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-04-03 02:36:11 +0100

    ws progress

--- lib/Classes/TheWebSocket.dart
+++ lib/Classes/TheWebSocket.dart
@@ -3,11 +3,11 @@ import 'package:web_socket_channel/status.dart' as status;
 
 class TheWebSocket {
   static void connect(){
-    var channel = WebSocketChannel.connect(Uri.parse('wss://echo.websocket.org'));
+    var channel = WebSocketChannel.connect(Uri.parse('wss://7anut.app'));
     print("connection established");
     channel.sink.add('hello server');
     channel.stream.listen((message) {
-      print("from server: " + message);
+      print(message);
       channel.sink.close(status.goingAway);
     }, onDone: (){
       print("connection is closed");
